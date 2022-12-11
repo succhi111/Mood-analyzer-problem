af1e618415059c8eb81fb312b834408f3bd5a43c
succhi111 <suchendramishra396@gmail.com>
2022-12-11 14:59:34 +0530

    Return sad mood

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,24 +1,15 @@
 package com.bridgelabz;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
-@Test
+  @Test
     public void givenMessage_WhenProper_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a sad message");
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
         System.out.println(mood);
-       Assert.assertEquals("SAD", mood);
-    }
-
-    @Test
-    public void givenMessage_WhenProper_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a happy message");
-        System.out.println(mood);
-        Assert.assertEquals("HAPPY", mood);
+      Assert.assertEquals("SAD", mood);
     }
 }
